d0c0ca3cf6b91398bf38e325fd65e722d62e10a6
王水胜 <602466011@qq.com>
2020-06-27 11:01:55 +0800

    添加:AA组件 测试

---
+++ src/views/modules/components/AA.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="box">
+      <div v-for="(item, index) in list" :key="index">
+        姓名: {{ item.name }}
+        <br>
+        年龄: {{ item.age }}
+      </div>
+
+      显示其中的某一个
+      <!-- {{ list[0]['name'] }} -->
+      解决办法
+      {{ list[0] && list[0]['name'] }}
+
+   </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {
+      list: {
+        type: Array,
+        default: () => []
+      }
+    },
+    data() {
+      return {
+          
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filters: {}
+  }
+</script>
+<style scoped>
+</style>
