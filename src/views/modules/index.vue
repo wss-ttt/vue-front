dc6b1c34479a0026bda616fc23967ad7bb61880f
王水胜 <602466011@qq.com>
2020-07-07 09:09:22 +0800

    更新: 新旧密码校验

--- src/views/modules/index.vue
+++ src/views/modules/index.vue
@@ -1,43 +1,78 @@
 <template>
-    <div class="wrapper">
-      <a-a :list="list" />
-   </div>
+  <div class="wrapper">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
-
 <script>
-  import AA from './components/AA'
   export default {
-    components: {
-      AA
-    },
-    props: {},
     data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
       return {
-         list: [] 
+        ruleForm: {
+          pass: '',
+          checkPass: '',
+          age: ''
+        },
+        rules: {
+          pass: [{
+            validator: validatePass,
+            trigger: 'blur'
+          }],
+          checkPass: [{
+            validator: validatePass2,
+            trigger: 'blur'
+          }]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
       }
-    },
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {
-      this.list = [{
-        name: 'aa',
-        age: 18
-      }, {
-        name: 'bb',
-        age: 28
-      }]
-    },
-    activated() {},
-    deactivated() {},
-    updated() {},
-    destroyed() {},
-    methods: {},
-    filters: {}
+    }
   }
+
 </script>
 <style scoped lang="scss">
-.wrapper {
+  .wrapper {}
 
-}
 </style>
