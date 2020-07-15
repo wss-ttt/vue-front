c406d0396ff5fc2d280aa2643660d77e6c610a59
王水胜 <602466011@qq.com>
2020-07-15 17:58:13 +0800

    添加：upload页面

---
+++ src/views/modules/test/upload.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <v-upload  @on-change="change"></v-upload>
+  </div>
+</template>
+
+<script>
+import VUpload from './components/upload'
+export default {
+  components: { VUpload },
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {
+    httpRequest() {
+      console.log('发送请求,进行上传文件')
+    },
+    change(fileList) {
+      console.log('fileList', fileList)
+    }
+  },
+  filters: {}
+}
+</script>
+<style scoped>
+</style>
