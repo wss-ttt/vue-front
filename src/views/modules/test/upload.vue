5919ccab129709721d568dd9364140e43bf76f64
王水胜 <602466011@qq.com>
2020-07-16 09:57:46 +0800

    更新：使用input type="file"编写上传功能

--- src/views/modules/test/upload.vue
+++ src/views/modules/test/upload.vue
@@ -1,35 +1,55 @@
 <template>
-  <div class="wrapper">
-    <v-upload  @on-change="change"></v-upload>
+  <div class="upload" @click="open">
+    <span>上传</span>
+    <input type="file" ref="uploadFile" @change="change($event)" />
   </div>
 </template>
 
 <script>
-import VUpload from './components/upload'
 export default {
-  components: { VUpload },
-  props: {},
-  data() {
-    return {}
+  components: {},
+  props: {
   },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {},
-  activated() {},
-  deactivated() {},
-  updated() {},
-  destroyed() {},
+  data() {
+    return {
+    }
+  },
   methods: {
-    httpRequest() {
-      console.log('发送请求,进行上传文件')
+    open() {
+      // this.$refs['uploadFile'].dispatchEvent(new MouseEvent('click'))
+
+      this.$refs['uploadFile'].click()
     },
-    change(fileList) {
-      console.log('fileList', fileList)
+    change(e) {
+      // 兼容性获取event对象
+      let event = e || window.event
+      // 兼容性获取target对象
+      let target = event.target || event.srcElement
+      const files = target.files
+      // 可以在这个地方对文件进行校验
+      if (files[0].size / 1024 / 1024 > 1 ) {
+        this.$message.error('选择的文件不能超过1M')
+        return
+      }
+      // 在这个地方可以进行发送请求,进行上传
+      this.uploadFile()
+    },
+    uploadFile() {
     }
   },
   filters: {}
 }
 </script>
-<style scoped>
+<style scoped lang="scss">
+.upload {
+  display: inline-block;
+  padding: 12px 20px;
+  background: #409eff;
+  color: #fff;
+  border-radius: 4px;
+  cursor: pointer;
+  input {
+    display: none;
+  }
+}
 </style>
