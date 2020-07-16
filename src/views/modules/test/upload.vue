5fdbcc3d7c279b5a232e0c53d2ace9664c12e901
王水胜 <602466011@qq.com>
2020-07-16 11:56:55 +0800

    更新: 使用type=file 实现简单的elememt-ui的upload组件功能

--- src/views/modules/test/upload.vue
+++ src/views/modules/test/upload.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="upload" @click="open">
-    <span>上传</span>
-    <input type="file" ref="uploadFile" @change="change($event)" />
+  <div class="wrapper">
+    <div class="upload" @click="open">
+      <span>上传</span>
+      <input type="file" ref="uploadFile" @change="change($event)" multiple />
+    </div>
+    <ul>
+      <li v-for="(item, index) in fileList" :key="index">{{ item.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   components: {},
-  props: {
-  },
+  props: {},
   data() {
     return {
+      fileList: [] // 存放文件
     }
   },
   methods: {
@@ -26,16 +31,16 @@ export default {
       // 兼容性获取target对象
       let target = event.target || event.srcElement
       const files = target.files
+      this.fileList = [].slice.call(files)
       // 可以在这个地方对文件进行校验
-      if (files[0].size / 1024 / 1024 > 1 ) {
+      if (files[0].size / 1024 / 1024 > 1) {
         this.$message.error('选择的文件不能超过1M')
         return
       }
       // 在这个地方可以进行发送请求,进行上传
       this.uploadFile()
     },
-    uploadFile() {
-    }
+    uploadFile() {}
   },
   filters: {}
 }
