0a4b555bac18c2995f6ada9bccdb0f7f0da13b61
wangshuisheng <602466011@qq.com>
2020-07-18 16:52:24 +0800

    更新: carousel走马灯的基本使用

---
+++ src/views/modules/test/carousel.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper">
+    <div class="block">
+      <button type="button" @click="autoplay = !autoplay">自动播放切换</button>
+      <el-carousel height="150px" :autoplay="autoplay" trigger="click">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+        autoplay: false
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filters: {}
+  }
+
+</script>
+<style scoped>
+  .el-carousel {
+    width: 100px;
+    border: 1px solid red;
+    margin: auto;
+  }
+
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+
+</style>
