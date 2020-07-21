3edfdd0208a359d15ab50a37354087d932d7ef6e
王水胜 <602466011@qq.com>
2020-07-21 17:37:35 +0800

    添加： vuex.vue页面

---
+++ src/views/modules/test/vuex.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    {{ homePageName }}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState({
+      homePageName: state => state
+    })
+  },
+  watch: {},
+  created() {},
+  mounted() {
+    console.log('aa', this.homePageName)
+  },
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filters: {}
+}
+</script>
+<style scoped>
+</style>
