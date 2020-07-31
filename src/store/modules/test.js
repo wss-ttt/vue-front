d9c0c51d6a5634051711fdf8ee8e47314eb8946d
王水胜 <602466011@qq.com>
2020-07-31 19:23:25 +0800

    更新: test.sj

--- src/store/modules/test.js
+++ src/store/modules/test.js
@@ -9,8 +9,23 @@ export default {
     }
   },
   mutations: {
+    // 不能做异步操作
     updateMsg (state, msg) {
+      console.log('aaa')
+
+      // 这个代码会报错
+      // setTimeout(() => {
+      //   state.msg = msg
+      // }, 1000)
+
       state.msg = msg
+    }
+  },
+  actions: {
+    updateMsg2 (context, msg) {
+      setTimeout(() => {
+        context.commit('updateMsg', msg)
+      }, 1000)
     }
   }
 }
